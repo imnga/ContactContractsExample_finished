725489ff85cdbd1c92ca6b05545f466244e4afce
ENZHE ABDULLINA <120475358+imnga@users.noreply.github.com>
2023-04-10 02:39:53 +0300

    ContactsContractExample finished

--- app/src/main/java/ru/synergy/contactcontractsexample/MainActivity.java
+++ app/src/main/java/ru/synergy/contactcontractsexample/MainActivity.java
@@ -1,14 +1,118 @@
 package ru.synergy.contactcontractsexample;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.loader.app.LoaderManager;
+import androidx.loader.content.CursorLoader;
+import androidx.loader.content.Loader;
 
+import android.content.ContentProvider;
+import android.content.ContentProviderOperation;
+import android.content.ContentProviderResult;
+import android.content.OperationApplicationException;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.os.RemoteException;
+import android.provider.ContactsContract;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+
+    private SimpleCursorAdapter adapter;
+    private ListView listView;
+    private Button show, save;
+    private EditText name, phone;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        listView = (ListView) findViewById(R.id.contactsList);
+        name = (EditText) findViewById(R.id.name);
+        phone = (EditText) findViewById(R.id.phone);
+
+        show = (Button) findViewById(R.id.show);
+        show.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                adapter = new SimpleCursorAdapter(getApplicationContext(), android.R.layout.simple_list_item_2, null,
+                        new String[]{ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,ContactsContract.CommonDataKinds.Phone.NUMBER},
+                        new int[]{android.R.id.text1,android.R.id.text2}, 0);//значение флагов в документации
+                listView.setAdapter(adapter);
+                LoaderManager.getInstance(MainActivity.this).initLoader(0, null, MainActivity.this);
+            }
+        });
+        save = (Button) findViewById(R.id.save);
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String contactName = name.getText().toString();
+                String contactPhone = phone.getText().toString();
+                addContact( contactName, contactPhone);
+                Toast.makeText(getApplicationContext(),"Contact added", Toast.LENGTH_LONG).show();
+            }
+        });
+    }
+
+    private void addContact(String contactName, String contactPhone){
+        ArrayList<ContentProviderOperation> ops = new ArrayList<>();
+        int rawContactsInsertIndex = ops.size();
+
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.RawContacts.CONTENT_URI)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_TYPE,null)
+                .withValue(ContactsContract.RawContacts.ACCOUNT_NAME,null)
+                .build());
+
+        ops.add(ContentProviderOperation
+                .newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID,rawContactsInsertIndex)
+                .withValue(ContactsContract.Data.MIMETYPE,ContactsContract.CommonDataKinds.StructuredName.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.StructuredName.DISPLAY_NAME,contactName)//выставили имя человека
+                .build());
+        ops.add(ContentProviderOperation.newInsert(ContactsContract.Data.CONTENT_URI)
+                .withValueBackReference(ContactsContract.Data.RAW_CONTACT_ID, rawContactsInsertIndex)
+                .withValue(ContactsContract.Data.MIMETYPE,ContactsContract.CommonDataKinds.Phone.CONTENT_ITEM_TYPE)
+                .withValue(ContactsContract.CommonDataKinds.Phone.NUMBER,contactPhone)
+                .withValue(ContactsContract.CommonDataKinds.Phone.TYPE,ContactsContract.CommonDataKinds.Phone.TYPE_MOBILE)
+                .build());
+
+        try{
+            ContentProviderResult[] res = getContentResolver().applyBatch(ContactsContract.AUTHORITY, ops);
+        }catch (RemoteException e){
+            e.printStackTrace();
+        }catch (OperationApplicationException e){
+            e.printStackTrace();
+        }
+    }
+
+    @NonNull
+    @Override
+    public Loader onCreateLoader(int id, @Nullable Bundle args) {
+        return new CursorLoader(getApplicationContext(),ContactsContract.CommonDataKinds.Phone.CONTENT_URI,new String[]{
+                ContactsContract.CommonDataKinds.Phone._ID,
+                ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME,
+                ContactsContract.CommonDataKinds.Phone.NUMBER,
+                ContactsContract.CommonDataKinds.Phone.PHOTO_ID
+        }, null, null, null);
+    }
+
+    @Override
+    public void onLoadFinished(@NonNull Loader loader, Cursor cursor) {
+        adapter.swapCursor(cursor);
+    }
+
+    @Override
+    public void onLoaderReset(@NonNull Loader loader) {
+        adapter.swapCursor(null);
     }
 }
